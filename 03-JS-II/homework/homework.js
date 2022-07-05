0addee16b17cb5c211b777f5479dc282a22ff098
juanmajustel <juanmajustel@gmail.com>
2022-07-05 20:23:35 -0300

    firts commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -1,10 +1,16 @@
-// No cambies los nombres de las funciones.
+ // No cambies los nombres de las funciones.
 
 function obtenerMayor(x, y) {
   // "x" e "y" son números enteros (int).
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+if (x > y){
+  return x;
+}else{
+  return y;
+}
+
 }
 
 function mayoriaDeEdad(edad) {
